27b11dedc56f17d374868217c842c3edf54b7fe4
snap <72922183+Snap385@users.noreply.github.com>
2024-12-08 21:26:07 +0500

    Update auth-screens.tsx

--- auth-screens.tsx
+++ auth-screens.tsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { SplashScreen } from '../components/SplashScreen'
+import { SplashScreen } from '@/components/SplashScreen'
 import { motion, AnimatePresence } from 'framer-motion'
 
 export default function AuthScreens() {
